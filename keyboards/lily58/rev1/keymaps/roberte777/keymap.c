0bb245f61f8b681d0b90be06c40dadbb46cb04db
roberte777 <rewilkes0041@gmail.com>
2024-08-27 04:12:34 -0500

    fix: left half master

--- keyboards/lily58/rev1/keymaps/roberte777/keymap.c
+++ keyboards/lily58/rev1/keymaps/roberte777/keymap.c
@@ -2,9 +2,7 @@
 
 enum layer_number {
   _QWERTY = 0,
-  _LOWER,
   _RAISE,
-  _ADJUST,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
--- keyboards/lily58/rev1/keymaps/roberte777/rules.mk
+++ keyboards/lily58/rev1/keymaps/roberte777/rules.mk
@@ -1,15 +1,18 @@
-LTO_ENABLE = yes            # Link Time Optimization enabled
+BOOTLOADER = atmel-dfu      # Required for Elite-C microcontrollers
 BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = no        # Mouse keys
 EXTRAKEY_ENABLE = no        # Audio control and System control
-CONSOLE_ENABLE = no         # Console for debug
+CONSOLE_ENABLE = yes        # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
-NKRO_ENABLE = no            # N-Key Rollover
+NKRO_ENABLE = no            # N-key rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 AUDIO_ENABLE = no           # Audio output
-RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.
+RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight
 SWAP_HANDS_ENABLE = no      # Enable one-hand typing
-OLED_ENABLE = yes           # OLED display
+OLED_ENABLE = no            # OLED display
+KEY_OVERRIDE_ENABLE = yes   # Enable key overrides feature
+EXTRAKEY_ENABLE = yes       # Enable media keys, etc.
+COMBO_ENABLE = yes          # Enable combos
 CONVERT_TO = promicro_rp2040
 
 # If you want to change the display of OLED, you need to change here
