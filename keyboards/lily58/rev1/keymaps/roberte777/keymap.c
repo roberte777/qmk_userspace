bd2e42c38b0c613e733ae2128c2984b21bced60b
roberte777 <rewilkes0041@gmail.com>
2024-09-13 01:59:29 -0500

    claude fix

--- keyboards/lily58/rev1/keymaps/roberte777/keymap.c
+++ keyboards/lily58/rev1/keymaps/roberte777/keymap.c
@@ -38,13 +38,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RGUI,
                            KC_LALT, KC_LGUI, KC_ENT, KC_SPC, KC_BSPC, MO(_RAISE), KC_ESC, KC_DEL
 ),
-
 [_RAISE] = LAYOUT(
      KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,    KC_F8,    KC_F9,    KC_F10,    KC_F11,    KC_F12,
-     KC_TILD, KC_EXLM, KC_AT,   KC_LBRC, KC_RBRC, KC_AMPR,                     KC_PLUS,    KC_7,    KC_8,    KC_9,    KC_ASTR,    XXXXXXX,
-     KC_GRV,  KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_UNDS,                     KC_MINS,    KC_4,    KC_5,    KC_6,    KC_SLSH, XXXXXXX,
-     KC_LCTL, KC_PERC, KC_CIRC, KC_LCBR, KC_RCBR, KC_PIPE, KC_LBRC,  KC_RBRC,  KC_EQL,    KC_1,    KC_2, KC_3,  KC_0,  XXXXXXX,
-                                XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, MO(_RAISE), KC_0, KC_PDOT
+     KC_TILD, KC_EXLM, KC_AT,   KC_LBRC, KC_RBRC, KC_AMPR,                   KC_PLUS,  KC_7,    KC_8,    KC_9,    KC_ASTR,    XXXXXXX,
+     KC_GRV,  KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_UNDS,                   KC_MINS,  KC_4,    KC_5,    KC_6,    KC_SLSH,    XXXXXXX,
+     KC_LCTL, KC_PERC, KC_CIRC, KC_LCBR, KC_RCBR, KC_PIPE, KC_LBRC, KC_RBRC, KC_EQL,   KC_1,    KC_2,    KC_3,    KC_0,       XXXXXXX,
+                                XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_TRNS,  KC_0,    KC_PDOT
 ),
 };
 
