bc9351adbdefaaaa41987b0edd7bc60e9952ea81
roberte777 <rewilkes0041@gmail.com>
2025-01-02 09:57:51 -0600

    feat: added arrow keys

--- keyboards/lily58/rev1/keymaps/roberte777/keymap.c
+++ keyboards/lily58/rev1/keymaps/roberte777/keymap.c
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_EQL,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
      KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
      KC_LSFT,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-     KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RGUI,
+     KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, MO(_LOWER),  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RGUI,
                            KC_LALT, KC_LGUI, KC_ENT, KC_SPC, KC_BSPC, MO(_RAISE), KC_ESC, KC_DEL
 ),
 [_RAISE] = LAYOUT(
@@ -44,6 +44,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_GRV,  KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_UNDS,                   KC_MINS,  KC_4,    KC_5,    KC_6,    KC_SLSH,    XXXXXXX,
      KC_LCTL, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_PIPE, KC_LBRC, KC_RBRC, KC_EQL,   KC_1,    KC_2,    KC_3,    KC_0,       XXXXXXX,
                                 XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_TRNS,  KC_0,    KC_PDOT
+),
+[_LOWER] = LAYOUT(
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, XXXXXXX, XXXXXXX,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                             _______, _______, _______, _______, _______,  _______, _______, _______
 ),
 };
 
